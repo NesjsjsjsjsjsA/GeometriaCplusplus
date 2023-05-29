83b3f90a0def1e39a784674d4e6811ae46a4bfa4
NesjsjsjsjsjsA <00133723@uca.edu.sv>
2023-05-29 14:34:10 -0600

    x.3

---
+++ Geo3,5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    cout<<endl;
+
+    for (int i = 0; i <= 1; i++)
+    {
+        cout<<"+";
+    }
+
+    cout<<endl;
+
+    for (int e = 0; e <= 1; e++)
+    {
+        cout<<" ";
+    }
+
+    cout<<"+";
+    
+    
+    return 0;
+}
